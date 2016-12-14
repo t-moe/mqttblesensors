1713067bffe75d1ef320f54114f31c29cc391c7f
steir1 <steir1@bfh.ch>
2016-12-14 13:24:57 +0100

    web: add comments

--- web/measure.js
+++ web/measure.js
@@ -1,55 +1,60 @@
 /*------------------------------------------------------------------------------+
 |	Javascript code for measure tab												|
 +------------------------------------------------------------------------------*/
-function dataArrived(data) { 
-	if ($("#tempChart").data("chart")){
+function dataArrived(data) { //new data from connected sensor arrived
+	if ($("#tempChart").data("chart")){	//only update chart if it already exists
 		var modifiedChart;
 		var modifiedDataArray;
 		
-		switch (data.type) {
+		switch (data.type) { //temperature data received
 			case "temperature":{
 				modifiedChart = $("#tempChart");
-				modifiedDataArray = $("#tempChart").data("array");
+				modifiedDataArray = modifiedChart.data("array");
 				
-				modifiedDataArray.addRow(['', data.raw]);
+				modifiedDataArray.addRow(['', data.raw]);	//add new temp data
 			}
 			break;
-			case "accelerate":{
-				modifiedDataArray = $("#accelChart").data("array");
+			case "accelerate":{ //accelerate data received
 				modifiedChart = $("#accelChart");
+				modifiedDataArray = modifiedChart.data("array"); 
 				
-				modifiedDataArray.addRow(['', data.raw.x, data.raw.y, data.raw.z]);
+				modifiedDataArray.addRow(['', data.raw.x, data.raw.y, data.raw.z]); //add new acceleration data
 			}
 			break;
-			case "gyro":{
-				modifiedDataArray = $("#gyroChart").data("array");
+			case "gyro":{ //gyro data received
 				modifiedChart = $("#gyroChart");
+				modifiedDataArray = modifiedChart.data("array"); 
 				
-				modifiedDataArray.addRow(['', data.raw.x, data.raw.y, data.raw.z]);
+				modifiedDataArray.addRow(['', data.raw.x, data.raw.y, data.raw.z]); //add new gyro data
 			}
 			break;
 		}
 		
+		/* clear oldest data value when buffer is full */
 		if(modifiedDataArray.getNumberOfRows() >= (recordTime * measuresPerSec)){
 			modifiedDataArray.removeRow(0);
 		}
+		
+		/* update chart */
 		if(modifiedChart.is(":visible")){
+			console.log(modifiedChart.data("options"));
 			modifiedChart.data("chart").draw(modifiedDataArray, modifiedChart.data("options"));
 		}
 	}
 }
 
-/* Acceleration Chart */
+/* initialize and draw acceleration chart */
 function drawChartAccel() {
-    var data = new google.visualization.DataTable();
+    /* generate new data table */
+	var data = new google.visualization.DataTable();
+	
+	/* chart configuration */
     data.addColumn('string', 'time');
     data.addColumn('number', 'X');
     data.addColumn('number', 'Y');
     data.addColumn('number', 'Z');
 	
-	data.addRows([
-				['', 0, 0, 0]
-				]);
+	data.addRow(['', 0, 0, 0]);
 	
     var options = {
         chart: {
@@ -59,6 +64,7 @@ function drawChartAccel() {
         height: 350
     };
 	
+	/* set up chart */
 	$("#accelChart").data("options", options);
 	$("#accelChart").data("array", data);
     var chart = new google.charts.Line($("#accelChart")[0]);
@@ -66,17 +72,18 @@ function drawChartAccel() {
 	$("#accelChart").data("chart", chart);
 }
 
-/* Gyroscope Chart */
+/* initialize and draw gyro chart */
 function drawChartGyro() {
+	/* generate new data table */
     var data = new google.visualization.DataTable();
+	
+	/* chart configuration */
     data.addColumn('string', 'time');
     data.addColumn('number', 'X');
     data.addColumn('number', 'Y');
     data.addColumn('number', 'Z');
 	
-	data.addRows([
-				['', 0, 0, 0]
-				]);
+	data.addRow(['', 0, 0, 0]);
 	
     var options = {
         chart: {
@@ -86,6 +93,7 @@ function drawChartGyro() {
         height: 350
     };
 	
+	/* set up chart */
     $("#gyroChart").data("options", options);
 	$("#gyroChart").data("array", data);
     var chart = new google.charts.Line($("#gyroChart")[0]);
@@ -93,15 +101,16 @@ function drawChartGyro() {
 	$("#gyroChart").data("chart", chart);
 }
 
-/* Temperature Chart */
+/* initialize and draw temperature chart */
 function drawChartTemp() {
+	/* generate new data table */
     var data = new google.visualization.DataTable();
+	
+	/* chart configuration */
     data.addColumn('string', 'time');
     data.addColumn('number', '°C');
 	
-	data.addRows([
-				['', 0]
-				]);
+	data.addRow(['', 0]);
 	
     var options = {
         chart: {
@@ -111,6 +120,7 @@ function drawChartTemp() {
         height: 350
     };
 	
+	/* set up chart */
 	$("#tempChart").data("options", options);
 	$("#tempChart").data("array", data);
     var chart = new google.charts.Line($("#tempChart")[0]);
